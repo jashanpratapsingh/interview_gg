5a25843d348a3b9fb7dbcd98ed71a9bad60b0fec
jashanpratapsingh <jashanpratap123@gmail.com>
2024-08-23 19:36:18 -0400

    Added the wallet connect to the app

--- client/src/components/loading/Loader.js
+++ client/src/components/loading/Loader.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import spinner from './../../assets/icons/spinning-circles.svg'
-import './Loader.scss'
+import React, { useState } from 'react';
+import spinner from './../../assets/icons/spinning-circles.svg';
+import './Loader.scss';
+import { ethers } from 'ethers';
+
+
+const [walletAddress, setWalletAddress] = useState("");
+
+// Helper Functions
+
+// Requests access to the user's META MASK WALLET
+// https://metamask.io
+async function requestAccount() {
+  console.log('Requesting account...');
+
+  // ❌ Check if Meta Mask Extension exists 
+  if(window.ethereum) {
+    console.log('detected');
+
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+    } catch (error) {
+      console.log('Error connecting...');
+    }
+
+  } else {
+    alert('Meta Mask not detected');
+  }
+}
+
+// Create a provider to interact with a smart contract
+async function connectWallet() {
+  if(typeof window.ethereum !== 'undefined') {
+    await requestAccount();
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+  }
+}
 
 const Loader = () => (
   <>
-    <img className="spinner-circles" src={spinner} />
+    {/* <img className="spinner-circles" src={spinner} /> */}
+    <button>Connect Wallet</button>
+    <h3>Wallet Address: 0xaaaa.....ccccc</h3>
   </>
 );
 
